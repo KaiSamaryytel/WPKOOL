71c356dfa2ca9102992c1f215a01af96baf0c667
kaikoppeliktkhk <kai.koppel@khk.ee>
2018-02-22 20:25:01 +0200

    modified file `wordfence-waf.php`

---
+++ wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('/home/kaikoppeliktkhk/public_html/wordpress/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", '/home/kaikoppeliktkhk/public_html/wordpress/wp-content/wflogs/');
+	include_once '/home/kaikoppeliktkhk/public_html/wordpress/wp-content/plugins/wordfence/waf/bootstrap.php';
+}
+?>
